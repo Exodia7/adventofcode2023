e8a14c704cd8ee02bd4bacd3650ac39dbb52d707
Exodia7 <valentin.ringlet@gmail.com>
2023-12-08 11:10:08 +0100

    Added more efficient approach

--- day08/part2.py
+++ day08/part2.py
@@ -7,6 +7,67 @@ def lcm(n1, n2):
     """
     return abs(n1*n2) // gcd(n1, n2)
 
+def computeNumStepsToFinalStateV1(startingStates, moves, transitions):
+    """ Computes the number of steps needed to reach a final state for each of the starting states individually.
+        Returns a list with the number of steps for each starting state.
+        
+        This version takes a simpler approach, performing the state transitions for one state at a time.
+        However, as a consequence, it is slightly less efficient.
+    """
+    currentStates = [state for state in startingStates]
+    numStepsToFinalState = [0 for state in startingStates]
+    # for each state,
+    for i in range(len(currentStates)):
+        # simulate the transitions until we reach a final state
+        while True:
+            # take next move and perform corresponding transition
+            nextMove = moves[numStepsToFinalState[i] % len(moves)]
+            currentStates[i] = transitions[(currentStates[i], nextMove)]
+            numStepsToFinalState[i] += 1
+            
+            # if the state is final, stop
+            if (currentStates[i].endswith("Z")):
+                break
+    
+    return numStepsToFinalState
+
+def computeNumStepsToFinalStateV2(startingStates, moves, transitions):
+    """ Computes the number of steps needed to reach a final state for each of the starting states individually.
+        Returns a list with the number of steps for each starting state.
+        
+        This version takes a more efficient, but also more complex approach than V1. It performs the simulations of each non-final state simultaneously.
+    """
+    isStateFinal = lambda state: state.endswith("Z")
+    currentStates = [state for state in startingStates]
+    numStepsToFinalState = [0 for state in startingStates]
+    numSteps = 0
+    # keep a list of the states which have not yet reached their final state
+    unfinishedStateIndices = list(range(len(startingStates)))
+    while True:
+        # find next move
+        nextMove = moves[numSteps % len(moves)]
+        numSteps += 1
+        # do the state transition for all states which are not yet final
+        i = 0
+        while i < len(unfinishedStateIndices):
+            stateIdx = unfinishedStateIndices[i]
+            currentStates[stateIdx] = transitions[(currentStates[stateIdx], nextMove)]
+            # check if we have now reached the final state
+            if isStateFinal(currentStates[stateIdx]):
+                numStepsToFinalState[stateIdx] = numSteps
+                # remove the state from the list
+                del unfinishedStateIndices[i]
+            else:
+                # go to next state only if we didn't remove a state from the list
+                i += 1
+        
+        # stop once we reached the end for all states
+        if len(unfinishedStateIndices) == 0:
+            break
+    
+    return numStepsToFinalState
+    
+
 INPUT_FILE = "input.txt"
 
 with open(INPUT_FILE, 'r') as f:
@@ -35,16 +96,9 @@ with open(INPUT_FILE, 'r') as f:
             startingStates.append(sourceState)
     
     # then, perform the state transitions for each separate state
-    currentStates = [state for state in startingStates]
-    numStepsToFinalState = [0 for state in startingStates]
-    for i in range(len(currentStates)):
-        while True:
-            nextMove = moves[numStepsToFinalState[i] % len(moves)]
-            currentStates[i] = transitions[(currentStates[i], nextMove)]
-            numStepsToFinalState[i] += 1
-            
-            if (currentStates[i].endswith("Z")):
-                break
+    numStepsToFinalState = computeNumStepsToFinalStateV2(startingStates, moves, transitions)
+    # ALTERNATIVE:
+    #numStepsToFinalState = computeNumStepsToFinalStateV1(startingStates, moves, transitions)
     
     # and the final result is then the least common multiple of these numbers
     result = lcm(numStepsToFinalState[0], numStepsToFinalState[1])
