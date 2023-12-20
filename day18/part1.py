8a7cae108f81048e5c3e685f201ed91b8777d18d
Exodia7 <valentin.ringlet@gmail.com>
2023-12-21 00:11:14 +0100

    Solved part 1 of day 18!

--- day18/part1.py
+++ day18/part1.py
@@ -2,8 +2,40 @@
 from typing import Union
 import copy
 
-INPUT_FILE = "test.txt"
-DEBUG = True
+INPUT_FILE = "input.txt"
+
+BOTTOM_LEFT_CORNER = "L"
+BOTTOM_RIGHT_CORNER = "J"
+TOP_LEFT_CORNER = "F"
+TOP_RIGHT_CORNER = "7"
+VERTICAL_CONNECT = "|"
+HORIZONTAL_CONNECT = "-"
+NO_CONNECT = "."
+
+DEBUG = False
+
+
+def generateEdgesFromMoves(data: list[list[Union[str, int]]]) -> list[tuple[int]]:
+    """ Based on the given moves to perform, returns a list of the positions of the edges """
+    currPos = (0, 0)
+    edges = [(0, 0)]
+    for i in range(len(data)):
+        # extract the next move to perform
+        direction = data[i][0]
+        numSteps = data[i][1]
+        # perform it to get the destination position
+        nextPos = performMove(currPos, direction, numSteps)
+        
+        # DEBUG
+        #if (DEBUG): print(f"Moving from {currPos} to {nextPos}")
+        
+        # add the new border to the list
+        edges.append(nextPos)
+        
+        # finally, update the current position
+        currPos = nextPos
+    
+    return edges
 
 def addNewColumnsToMap(map: list[list[bool]], index: int, numNewColumns: int) -> list[list[bool]]:
     """ Adds the specified number of new columns at the specified index in the map """
@@ -85,6 +117,36 @@ def printBorders(bordersMap: list[list[bool]]):
                 print(".", end='')
         print()
 
+def computeConnector(bordersMap: list[list[bool]], pos: tuple[int]) -> str:
+    """ Computes what type of connecting piece this position would be """
+    x, y = pos
+    
+    # Check which of the sides is connected from top, bottom, left, right
+    isTopConnected = False 
+    if (y > 0): isTopConnected = bordersMap[y-1][x]
+    isBottomConnected = False
+    if (y < len(bordersMap)-1): isBottomConnected = bordersMap[y+1][x]
+    isLeftConnected = False
+    if (x > 0): isLeftConnected = bordersMap[y][x-1]
+    isRightConnected = False
+    if (x < len(bordersMap[0])-1): isRightConnected = bordersMap[y][x+1]
+    
+    # return the correct symbol
+    if (isTopConnected and isRightConnected):
+        return BOTTOM_LEFT_CORNER
+    elif (isTopConnected and isLeftConnected):
+        return BOTTOM_RIGHT_CORNER
+    elif (isBottomConnected and isRightConnected):
+        return TOP_LEFT_CORNER
+    elif (isBottomConnected and isLeftConnected):
+        return TOP_RIGHT_CORNER
+    elif (isTopConnected and isTopConnected):
+        return VERTICAL_CONNECT
+    elif (isLeftConnected and isRightConnected):
+        return HORIZONTAL_CONNECT
+    else:
+        return NO_CONNECT
+
 def computeSpaceOccupiedBy(bordersMap: list[list[bool]]) -> int:
     """ Computes the size of the shape described by the edges given as parameter, including the edge lines """
     if (DEBUG): print(f"Size of map: {len(bordersMap)}x{len(bordersMap[0])}")
@@ -97,14 +159,32 @@ def computeSpaceOccupiedBy(bordersMap: list[list[bool]]) -> int:
             if bordersMap[y][x]:
                 # add one for this space
                 totalSize += 1
+                ''' NOTE: the logic for this, computing if we're inside or not, 
+                    is largely based on part2.py of day10
+                '''
+                startConnector = computeConnector(bordersMap, (x, y))
                 
-                # continue up until the first empty space (or end of the line)
-                while (x < len(bordersMap[y])-1 and bordersMap[y][x+1]):
-                    totalSize += 1
-                    x += 1
-                
-                # and then invert the side we are on
-                inside = not inside
+                if startConnector == VERTICAL_CONNECT:
+                    # in case we cross a vertical border, we go from inside to outside or the other way around
+                    inside = not inside
+                else:
+                    # we have a corner, so it's more complex
+                    sameSideIsOnSide = "left"   # should be a TOP_LEFT_CORNER
+                    if startConnector == BOTTOM_LEFT_CORNER:
+                        sameSideIsOnSide = "right"
+                    
+                    # continue up until the first empty space (or end of the line)
+                    while (x < len(bordersMap[y])-1 and bordersMap[y][x+1]):
+                        totalSize += 1
+                        x += 1
+                    endConnector = computeConnector(bordersMap, (x, y))
+                    
+                    # and then check if we remain on the same side or not
+                    if ((endConnector == BOTTOM_RIGHT_CORNER and sameSideIsOnSide != "right") 
+                        or 
+                        (endConnector == TOP_RIGHT_CORNER and sameSideIsOnSide != "left")):
+                        # invert whether we're inside or not
+                        inside = not inside
             
             elif inside:
                 totalSize += 1
@@ -113,28 +193,49 @@ def computeSpaceOccupiedBy(bordersMap: list[list[bool]]) -> int:
     
     return totalSize
 
-def generateEdgesFromMoves(data: list[list[Union[str, int]]]) -> list[tuple[int]]:
-    """ Based on the given moves to perform, returns a list of the positions of the edges """
-    currPos = (0, 0)
-    edges = [(0, 0)]
-    for i in range(len(data)):
-        # extract the next move to perform
-        direction = data[i][0]
-        numSteps = data[i][1]
-        # perform it to get the destination position
-        nextPos = performMove(currPos, direction, numSteps)
-        
-        # DEBUG
-        #if (DEBUG): print(f"Moving from {currPos} to {nextPos}")
-        
-        # add the new border to the list
-        edges.append(nextPos)
-        
-        # finally, update the current position
-        currPos = nextPos
+def fillInsideSpace(originalBordersMap: list[list[bool]]) -> list[list[bool]]:
+    """ Replace all positions inside of the space by True values """
+    bordersMap = copy.deepcopy(originalBordersMap)
+    for y in range(len(bordersMap)):
+        inside = False
+        x = 0
+        while (x < len(bordersMap[y])):
+            if bordersMap[y][x]:
+                ''' NOTE: the logic for this, computing if we're inside or not, 
+                    is largely based on part2.py of day10
+                '''
+                startConnector = computeConnector(originalBordersMap, (x, y))
+                
+                if startConnector == VERTICAL_CONNECT:
+                    # in case we cross a vertical border, we go from inside to outside or the other way around
+                    inside = not inside
+                else:
+                    # we have a corner, so it's more complex
+                    sameSideIsOnSide = "left"   # should be a TOP_LEFT_CORNER
+                    if startConnector == BOTTOM_LEFT_CORNER:
+                        sameSideIsOnSide = "right"
+                    
+                    # continue up until the first empty space (or end of the line)
+                    while (x < len(bordersMap[y])-1 and bordersMap[y][x+1]):
+                        x += 1
+                    endConnector = computeConnector(originalBordersMap, (x, y))
+                    
+                    # and then check if we remain on the same side or not
+                    if ((endConnector == BOTTOM_RIGHT_CORNER and sameSideIsOnSide != "right") 
+                        or 
+                        (endConnector == TOP_RIGHT_CORNER and sameSideIsOnSide != "left")):
+                        # invert whether we're inside or not
+                        inside = not inside
+            
+            elif inside:
+                bordersMap[y][x] = True
+            
+            x += 1
     
-    return edges
-    
+    return bordersMap
+
+
+
 
 with open(INPUT_FILE, 'r') as f:
     # 1) parse the data
@@ -144,12 +245,6 @@ with open(INPUT_FILE, 'r') as f:
         data[i][1] = int(data[i][1])
         # remove the superfluous parts from the hex color
         data[i][2] = data[i][2][2:-1]
-    
-    # DEBUG
-    if (DEBUG):
-        print("ORIGINAL DATA:")
-        print(data)
-        print("\n----------------------\n")
     
     # 2) get all edge points
     edges = generateEdgesFromMoves(data)
@@ -169,12 +264,6 @@ with open(INPUT_FILE, 'r') as f:
         currX, currY = edges[i]
         edges[i] = (currX + incrementX, currY + incrementY)
     
-    # DEBUG
-    if (DEBUG):
-        print("EDGES:")
-        print(edges)
-        print("\n----------------------\n")
-    
     # 3) create the map with the border
     borders = createMapWithBorderOf(edges)
     
@@ -185,11 +274,23 @@ with open(INPUT_FILE, 'r') as f:
         printBorders(borders)
         print("\n----------------------\n")
     
-    # 3) compute how many tiles are on the border and inside
+    # 4) compute how many tiles are on the border and inside
     terrainSize = computeSpaceOccupiedBy(borders)
+    # 5) give the result of first approach
+    print(f"Approach 1: \nExactly {terrainSize} cubic meters of lava fit in the lagoon\n")
+        
+    # 5) compute the map where the inside is filled in
+    filledInMap = fillInsideSpace(borders)
     
-    # 4) return the result
-    print(f"Exactly {terrainSize} cubic meters of lava fit in the lagoon")
-    # TRIES: 295049 (too high),
-    #       104152 (too high)
-    #       49372 (incorrect)
+    # DEBUG
+    if (DEBUG):
+        print("BORDERS MAP FILLED INSIDE:")
+        printBorders(filledInMap)
+        print("\n----------------------\n")
+    
+    # 6) count up the number of filled in spaces
+    numberFilledInSpaces = sum([sum(row) for row in filledInMap])
+    
+    # 7) return the result for the second approach
+    print(f"Approach 2: \nNumber of filled-in fields = {numberFilledInSpaces}")
+    # ANSWER: 50465
