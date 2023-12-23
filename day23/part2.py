a3a94bedee08a7a5524d9c40efcb5373f8f99d95
Exodia7 <valentin.ringlet@gmail.com>
2023-12-23 20:57:56 +0100

    Another failed attempt at speeding search up
    I added the @lru_cache construct to cache results of the
    computePossibleNextPositions function.
    But also I tried to speed up by checking if we already encountered a
    node by only checking the last node and the crossroad nodes encountered
    during the path... but that slowed it down by a lot

--- day23/part2.py
+++ day23/part2.py
@@ -1,5 +1,6 @@
 
 import copy
+from functools import lru_cache
 
 INPUT_FILE = "input.txt"
 PATH = "."
@@ -7,32 +8,54 @@ FOREST = "#"
 SLOPES = ["^", ">", "v", "<"]
 VERBOSE = True
 
-def computePossibleNextPositions(map: list[str], currPos: tuple[int]) -> list[tuple[int]]:
+@lru_cache
+def computePossibleNextPositions(map: tuple[str], currPos: tuple[int]) -> list[tuple[int]]:
     """ Find all neighboring positions we can move to """
     currX, currY = currPos
     currSymbol = map[currY][currX]
     
     if currSymbol in SLOPES:
         # then we can only move downhill (in the direction of the slope)
+        possiblePosition = None
+        ''' # GOING IN THE OPPOSITE DIRECTION COMPARED TO THE SLOPE DIRECTION
         match currSymbol:
             case "^":
-                return [(currX, currY-1)]
+                possiblePosition = [(currX, currY+1)]
             case ">":
-                return [(currX+1, currY)]
+                possiblePosition = [(currX-1, currY)]
             case "v":
-                return [(currX, currY+1)]
+                possiblePosition = [(currX, currY-1)]
             case "<":
-                return [(currX-1, currY)]
+                possiblePosition = [(currX+1, currY)]
+        ''' #GOING IN THE DIRECTION OF THE SLOPE
+        match currSymbol:
+            case "^":
+                possiblePosition = [(currX, currY-1)]
+            case ">":
+                possiblePosition = [(currX+1, currY)]
+            case "v":
+                possiblePosition = [(currX, currY+1)]
+            case "<":
+                possiblePosition = [(currX-1, currY)]
+
+        return possiblePosition
+        '''
+        # only return it if it has not yet been visited
+        if possiblePosition not in visited:
+            return possiblePosition
+        else:
+            return []
+        '''
     else:
         # then we can move in each direction, assuming the tile is not forest
         possiblePositions = []
-        if (currX > 0 and map[currY][currX-1] != FOREST):
-            possiblePositions.append((currX-1, currY))
-        if (currX < len(map[currY])-1 and map[currY][currX+1] != FOREST):
+        if (currX > 0 and map[currY][currX-1] != FOREST):   # and (currX-1, currY) not in visited):
+                possiblePositions.append((currX-1, currY))
+        if (currX < len(map[currY])-1 and map[currY][currX+1] != FOREST):   # and (currX+1, currY) not in visited):
             possiblePositions.append((currX+1, currY))
-        if (currY > 0 and map[currY-1][currX]!= FOREST):
+        if (currY > 0 and map[currY-1][currX]!= FOREST):    # and (currX, currY-1) not in visited):
             possiblePositions.append((currX, currY-1))
-        if (currY < len(map)-1 and map[currY+1][currX] != FOREST):
+        if (currY < len(map)-1 and map[currY+1][currX] != FOREST):  # and (currX, currY+1) not in visited):
             possiblePositions.append((currX, currY+1))
         ''' ALTERNATIVE
         possiblePositions = [(currX-1, currY), (currX+1, currY), (currX, currY-1), (currX, currY+1)]
@@ -40,9 +63,10 @@ def computePossibleNextPositions(map: list[str], currPos: tuple[int]) -> list[tu
         while (i < len(possiblePositions)):
             x, y = possiblePositions[i]
             if (y < 0 or y >= len(map) or x < 0 or x >= len(map)) \
-                or (map[y][x] == FOREST):
-                # we can not move to that position, either because we would move out of the map
-                # or because we would move to a forest tile
+                or (map[y][x] == FOREST) \
+                or ((x, y) in visited):
+                # we can not move to that position, either because we would move out of the map,
+                # because we would move to a forest tile or because we already visited the tile
                 del possiblePositions[i]
             else:
                 # keep current position, and move to next
@@ -52,18 +76,21 @@ def computePossibleNextPositions(map: list[str], currPos: tuple[int]) -> list[tu
         # return the valid possible positions
         return possiblePositions
 
-def findLongestPath(map: list[str], startPos: tuple[int], endPos: tuple[int], neighborsCache: dict[tuple[int], list[tuple[int]]], pathSoFar: list[tuple[int]]):
+def findLongestPath(map: list[str], startPos: tuple[int], endPos: tuple[int]):          #, neighborsCache: dict[tuple[int], list[tuple[int]]], pathSoFar: list[tuple[int]]):
     """ Finds the longest path from the starting position to the end position while respecting the conditions """
     maxPathLength = 0
     longestPath = []
-    pathsToExploreFurther = [[startPos]]
+    pathsToExploreFurther = [([startPos], [])]
+    lastPos = (-1, -1)
+    lastLastPos = (-1, -1)
     while len(pathsToExploreFurther) > 0:
         # 1) take the first path
-        currPath = pathsToExploreFurther.pop(0)
+        currPath, currVisited = pathsToExploreFurther.pop(0)
+        lastLastPos = lastPos
         lastPos = currPath[-1]
         
         # VERBOSE: print when we reach a new path length
-        if (VERBOSE and len(currPath) >= maxPathLength+250):
+        if (VERBOSE and len(currPath) >= maxPathLength+1):    #250):
             maxPathLength = len(currPath)
             print(f"Reached path length of {maxPathLength}")
         
@@ -74,18 +101,22 @@ def findLongestPath(map: list[str], startPos: tuple[int], endPos: tuple[int], ne
         else:
             # 3) go one step further
             # 3.1) compute next possible positions from current position
-            #nextPositions = computePossibleNextPositions(map, lastPos)
-            nextPositions = neighborsCache[lastPos]
+            nextPositions = computePossibleNextPositions(tuple(map), lastPos)   #, tuple(currPath))
+            #nextPositions = neighborsCache[lastPos]
+            # 3.2) append the node to the visited nodes in case it is at a crossroad
+            #       --> it can only occur again if it was at a crossroad
+            if len(nextPositions) > 2:
+                currVisited.append(lastPos)
             
-            # 3.2) go over all possible positions and add one new path to explore for each valid position
+            # 3.3) go over all possible positions and add one new path to explore for each valid position
             for i in range(len(nextPositions)):
                 # check that we don't go back to a position we already went to
                 nextPos = nextPositions[i]
-                if nextPos not in currPath:
+                if nextPos != lastLastPos and nextPos not in currVisited:
                     # if so, add the new path to the list to explore
                     nextPath = copy.copy(currPath)
                     nextPath.append(nextPos)
-                    pathsToExploreFurther.append(nextPath)
+                    pathsToExploreFurther.append((nextPath, copy.copy(currVisited)))
             
     # once we have explored everything, return the longest found path
     return longestPath
@@ -100,7 +131,7 @@ def precomputeNeighborPositions(map: list[str]) -> dict[tuple[int], list[tuple[i
             if map[y][x] != FOREST:
                 # compute neighbors and save them
                 pos = (x, y)
-                neighborsCache[pos] = computePossibleNextPositions(map, pos)
+                neighborsCache[pos] = computePossibleNextPositions(tuple(map), pos)
     
     return neighborsCache
                 
@@ -115,9 +146,8 @@ with open(INPUT_FILE, 'r') as f:
     for i in range(len(data)):
         for SLOPE_SYMBOL in SLOPES:
             data[i] = data[i].replace(SLOPE_SYMBOL, PATH)
-    
     # 2) pre-compute all neighbor nodes
-    neighborsCache = precomputeNeighborPositions(data)
+    #neighborsCache = precomputeNeighborPositions(data)
     
     # 3) find starting position
     y = 0
@@ -139,7 +169,7 @@ with open(INPUT_FILE, 'r') as f:
     endPos = (x, y)
     
     # 5) find longest path from start to end
-    longestPath = findLongestPath(data, startPos, endPos, neighborsCache, [])
+    longestPath = findLongestPath(data, startPos, endPos)   #neighborsCache, [])
     
     # leave an empty space
     if VERBOSE: print()
