ba9f9027f2f4cc72963c961ac04ca514677fa7df
Exodia7 <valentin.ringlet@gmail.com>
2023-12-23 19:15:47 +0100

    Trying cache of neighbors, not really working

--- day23/part2.py
+++ day23/part2.py
@@ -25,6 +25,16 @@ def computePossibleNextPositions(map: list[str], currPos: tuple[int]) -> list[tu
                 return [(currX-1, currY)]
     else:
         # then we can move in each direction, assuming the tile is not forest
+        possiblePositions = []
+        if (currX > 0 and map[currY][currX-1] != FOREST):
+            possiblePositions.append((currX-1, currY))
+        if (currX < len(map[currY])-1 and map[currY][currX+1] != FOREST):
+            possiblePositions.append((currX+1, currY))
+        if (currY > 0 and map[currY-1][currX]!= FOREST):
+            possiblePositions.append((currX, currY-1))
+        if (currY < len(map)-1 and map[currY+1][currX] != FOREST):
+            possiblePositions.append((currX, currY+1))
+        ''' ALTERNATIVE
         possiblePositions = [(currX-1, currY), (currX+1, currY), (currX, currY-1), (currX, currY+1)]
         i = 0
         while (i < len(possiblePositions)):
@@ -37,11 +47,12 @@ def computePossibleNextPositions(map: list[str], currPos: tuple[int]) -> list[tu
             else:
                 # keep current position, and move to next
                 i += 1
+        '''
         
         # return the valid possible positions
         return possiblePositions
 
-def findLongestPath(map: list[str], startPos: tuple[int], endPos: tuple[int], pathSoFar: list[tuple[int]]):
+def findLongestPath(map: list[str], startPos: tuple[int], endPos: tuple[int], neighborsCache: dict[tuple[int], list[tuple[int]]], pathSoFar: list[tuple[int]]):
     """ Finds the longest path from the starting position to the end position while respecting the conditions """
     maxPathLength = 0
     longestPath = []
@@ -63,7 +74,8 @@ def findLongestPath(map: list[str], startPos: tuple[int], endPos: tuple[int], pa
         else:
             # 3) go one step further
             # 3.1) compute next possible positions from current position
-            nextPositions = computePossibleNextPositions(map, lastPos)
+            #nextPositions = computePossibleNextPositions(map, lastPos)
+            nextPositions = neighborsCache[lastPos]
             
             # 3.2) go over all possible positions and add one new path to explore for each valid position
             for i in range(len(nextPositions)):
@@ -78,6 +90,21 @@ def findLongestPath(map: list[str], startPos: tuple[int], endPos: tuple[int], pa
     # once we have explored everything, return the longest found path
     return longestPath
         
+def precomputeNeighborPositions(map: list[str]) -> dict[tuple[int], list[tuple[int]]]:
+    """ Pre-compute a dictionary which at every non-forest node,
+        stores the non-forest nodes above, below, left and right of it
+    """
+    neighborsCache = {}
+    for y in range(len(map)):
+        for x in range(len(map)):
+            if map[y][x] != FOREST:
+                # compute neighbors and save them
+                pos = (x, y)
+                neighborsCache[pos] = computePossibleNextPositions(map, pos)
+    
+    return neighborsCache
+                
+
 
 
 
@@ -89,7 +116,10 @@ with open(INPUT_FILE, 'r') as f:
         for SLOPE_SYMBOL in SLOPES:
             data[i] = data[i].replace(SLOPE_SYMBOL, PATH)
     
-    # 2) find starting position
+    # 2) pre-compute all neighbor nodes
+    neighborsCache = precomputeNeighborPositions(data)
+    
+    # 3) find starting position
     y = 0
     x = 0
     while (x < len(data[y])):
@@ -98,7 +128,7 @@ with open(INPUT_FILE, 'r') as f:
         else:
             x += 1
     startPos = (x, y)
-    # 3) find target position
+    # 4) find target position
     y = len(data)-1
     x = 0
     while (x < len(data[y])):
@@ -108,12 +138,12 @@ with open(INPUT_FILE, 'r') as f:
             x += 1
     endPos = (x, y)
     
-    # 4) find longest path from start to end
-    longestPath = findLongestPath(data, startPos, endPos, [])
+    # 5) find longest path from start to end
+    longestPath = findLongestPath(data, startPos, endPos, neighborsCache, [])
     
     # leave an empty space
     if VERBOSE: print()
     
-    # 5) give the length of that path
+    # 6) give the length of that path
     print(f"The longest path has exactly {len(longestPath)-1} steps")
     # ANSWER: too freaking slow
